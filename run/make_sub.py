4c3bd1b4a9ad0d8b6dc73db0e3f065198b69ddf3
Ellariel <ell.science@mail.ru>
2023-06-24 02:30:00 +0200

    m

--- run/make_sub.py
+++ run/make_sub.py
@@ -2,6 +2,7 @@ import os
 
 subgraphs = [50, 60, 70, 80, 90, 100]
 idxs = list(range(5))
+n_envs = 1
 log = '#'
 output = '#'
 error = ''
@@ -9,14 +10,14 @@ error = ''
 for subgraph in subgraphs:
     for idx in idxs:
         line = f'''executable = /bin/bash 
-arguments = -i conda_activate.sh baselines train.py --subgraph {subgraph} --idx {idx}
+arguments = -i conda_activate.sh baselines train.py --n_envs {n_envs} --subgraph {subgraph} --idx {idx}
 transfer_input_files = conda_activate.sh, train.py 
-{log}log = ./logs/train-{subgraph}-{idx}.txt 
-{output}output = ./logs/train-stdout-{subgraph}-{idx}.txt
-{error}error = ./logs/train-stderr-{subgraph}-{idx}.txt
+{log}log = ./logs/train-{n_envs}-{subgraph}-{idx}.txt 
+{output}output = ./logs/train-stdout-{n_envs}-{subgraph}-{idx}.txt
+{error}error = ./logs/train-stderr-{n_envs}-{subgraph}-{idx}.txt
 should_transfer_files = IF_NEEDED 
 request_gpus = 1
 queue
 '''
-        with open(f'train-{subgraph}-{idx}.sub', 'wt') as file:
+        with open(f'run/train-{n_envs}-{subgraph}-{idx}.sub', 'wt') as file:
             file.writelines(line)
