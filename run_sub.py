fe85943adf342791bb09c9eed876817aef4386d6
Danila <d.v.valko@gmail.com>
2023-10-27 00:04:58 +0200

    m

--- run_sub.py
+++ run_sub.py
@@ -1,7 +1,7 @@
 import os, time
 
 subgraphs = [80, 90, 100] #[50, 60, 70]
-idxs = list(range(1, 4))
+idxs = list(range(0, 4))
 n_envs = 16
 log = '#'
 output = '#'
@@ -24,4 +24,5 @@ queue
         time.sleep(1)
         print(f'{n_envs}-{subgraph}-{idx}')
         os.system(f'condor_submit ./train-{n_envs}-{subgraph}-{idx}.sub')
+        time.sleep(1)
         os.remove(f'./train-{n_envs}-{subgraph}-{idx}.sub')
